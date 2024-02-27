a05d08a9b2bc922212e06cfdf0cfab6f215077c0
Selva-Torus <selvakumarg@torus.tech>
2024-02-27 15:28:24 +0530

    new changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
 "use client"
 import Demo from "@/PageComponents/Demo";
 import Login from "@/PageComponents/Login";
+import { useRouter } from "next/navigation";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/tailwind-light/theme.css";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(()=>{
+    router.push('datatable')
+  },[])
+
   return (
     <main >
       {/* <Login/> */}
